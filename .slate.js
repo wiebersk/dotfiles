299b3f694e492676e1105989027781991e0a3295
Kyle Wiebers <kyle@wiebe.rs>
2013-03-14 22:16:11 -0500

    Removed functionality from .slate.js

--- .slate.js
+++ .slate.js
@@ -7,9 +7,8 @@ S.cfga({
 });
 
 // Monitors
-var monLaptop = "1680x1050";
-var monTbolt = "2560x1440";
-var monHP = "1920x1080";
+var monLaptop = "1440x900";
+var monLg = "1920x1080";
 
 // Operations
 var lapChat = S.op("corner", {
@@ -19,25 +18,22 @@ var lapChat = S.op("corner", {
   "height" : "screenSizeY"
 });
 var lapMain = lapChat.dup({ "direction" : "top-right", "width" : "8*screenSizeX/9" });
-var tboltFull = S.op("move", {
-  "screen" : monTbolt,
+var lgFull = S.op("move", {
+  "screen" : monLg,
   "x" : "screenOriginX",
   "y" : "screenOriginY",
   "width" : "screenSizeX",
   "height" : "screenSizeY"
 });
-var tboltTop = tboltFull.dup({ "height" : "screenSizeY/2" });
-var tboltTopLeft = tboltTop.dup({ "width" : "screenSizeX/2" });
-var tboltTopRight = tboltTopLeft.dup({ "x" : "screenOriginX+screenSizeX/2" });
-var tboltBottom = tboltTop.dup({ "y" : "screenOriginY+screenSizeY/2" });
-var tboltBottomLeft = tboltBottom.dup({ "width" : "screenSizeX/3" });
-var tboltBottomMid = tboltBottomLeft.dup({ "x" : "screenOriginX+screenSizeX/3" });
-var tboltBottomRight = tboltBottomLeft.dup({ "x" : "screenOriginX+2*screenSizeX/3" });
-var tboltLeft = tboltTopLeft.dup({ "height" : "screenSizeY" });
-var tboltRight = tboltTopRight.dup({ "height" : "screenSizeY" });
-var hpTopLeft = tboltTopLeft.dup({ "screen" : monHP });
-var hpBottomLeft = hpTopLeft.dup({ "y" : "screenOriginY+screenSizeY/2" });
-var hpRight = tboltRight.dup({ "screen" : monHP });
+var lgTop = lgFull.dup({ "height" : "screenSizeY/2" });
+var lgTopLeft = lgTop.dup({ "width" : "screenSizeX/2" });
+var lgTopRight = lgTopLeft.dup({ "x" : "screenOriginX+screenSizeX/2" });
+var lgBottom = lgTop.dup({ "y" : "screenOriginY+screenSizeY/2" });
+var lgBottomLeft = lgBottom.dup({ "width" : "screenSizeX/3" });
+var lgBottomMid = lgBottomLeft.dup({ "x" : "screenOriginX+screenSizeX/3" });
+var lgBottomRight = lgBottomLeft.dup({ "x" : "screenOriginX+2*screenSizeX/3" });
+var lgLeft = lgTopLeft.dup({ "height" : "screenSizeY" });
+var lgRight = lgTopRight.dup({ "height" : "screenSizeY" });
 
 // common layout hashes
 var lapMainHash = {
@@ -45,22 +41,12 @@ var lapMainHash = {
   "ignore-fail" : true,
   "repeat" : true
 };
-var adiumHash = {
-  "operations" : [lapChat, lapMain],
-  "ignore-fail" : true,
-  "title-order" : ["Contacts"],
-  "repeat-last" : true
-};
-var tboltTopHash  = {
-  "operations" : [tboltTop],
-  "repeat" : true
-};
-var mvimHash = {
-  "operations" : [tboltTopLeft, tboltTopRight],
+var lgTopHash  = {
+  "operations" : [lgTop],
   "repeat" : true
 };
 var iTermHash = {
-  "operations" : [tboltBottomLeft, tboltBottomMid, tboltBottomRight, lapMain],
+  "operations" : [lgBottomLeft, lgBottomMid, lgBottomRight, lapMain],
   "sort-title" : true,
   "repeat-last" : true
 };
@@ -79,121 +65,49 @@ var genBrowserHash = function(regex) {
   };
 };
 
-// 3 monitor layout
-var threeMonitorLayout = S.lay("threeMonitor", {
-  "Adium" : {
-    "operations" : [lapChat, hpBottomLeft],
-    "ignore-fail" : true,
-    "title-order" : ["Contacts"],
-    "repeat-last" : true
-  },
-  "MacVim" : mvimHash,
-  "iTerm" : iTermHash,
-  "Google Chrome" : genBrowserHash(/^Developer\sTools\s-\s.+$/),
-  "Xcode" : {
-    "operations" : [tboltTop, hpRight],
-    "main-first" : true,
-    "repeat-last" : true
-  },
-  "Flex Builder" : tboltTopHash,
-  "GitX" : {
-    "operations" : [hpRight],
-    "repeat" : true
-  },
-  "Ooyala Player Debug Console" : {
-    "operations" : [hpBottomLeft],
-    "repeat" : true
-  },
-  "Firefox" : genBrowserHash(/^Firebug\s-\s.+$/),
-  "Safari" : lapMainHash,
-  "Eclipse" : tboltTopHash,
-  "Spotify" : {
-    "operations" : [hpTopLeft],
-    "repeat" : true
-  }
-});
-
-// 2 monitor layout
-var twoMonitorLayout = S.lay("twoMonitor", {
-  "Adium" : adiumHash,
-  "MacVim" : mvimHash,
-  "iTerm" : iTermHash,
-  "Google Chrome" : lapMainHash,
-  "Xcode" : {
-    "operations" : [tboltTop, lapMain],
-    "main-first" : true,
-    "repeat-last" : true
-  },
-  "Flex Builder" : tboltTopHash,
-  "GitX" : lapMainHash,
-  "Ooyala Player Debug Console" : lapMainHash,
-  "Firefox" : lapMainHash,
-  "Safari" : lapMainHash,
-  "Eclipse" : tboltTopHash,
-  "Spotify" : lapMainHash
-});
-
-// 1 monitor layout
-var oneMonitorLayout = S.lay("oneMonitor", {
-  "Adium" : adiumHash,
-  "MacVim" : lapMainHash,
-  "iTerm" : lapMainHash,
-  "Google Chrome" : lapMainHash,
-  "Xcode" : lapMainHash,
-  "Flex Builder" : lapMainHash,
-  "GitX" : lapMainHash,
-  "Ooyala Player Debug Console" : lapMainHash,
-  "Firefox" : lapMainHash,
-  "Safari" : lapMainHash,
-  "Eclipse" : lapMainHash,
-  "Spotify" : lapMainHash
-});
-
 // Defaults
-S.def([monHP, monTbolt, monLaptop], threeMonitorLayout);
-S.def([monTbolt, monLaptop], twoMonitorLayout);
-S.def([monLaptop], oneMonitorLayout);
+// S.def([monLg, monLaptop], twoMonitorLayout);
+// S.def([monLaptop], oneMonitorLayout);
 
 // Layout Operations
-var threeMonitor = S.op("layout", { "name" : threeMonitorLayout });
-var twoMonitor = S.op("layout", { "name" : twoMonitorLayout });
-var oneMonitor = S.op("layout", { "name" : oneMonitorLayout });
-var universalLayout = function() {
-  // Should probably make sure the resolutions match but w/e
-  if (S.screenCount() === 3) {
-    threeMonitor.run();
-  } else if (S.screenCount() === 2) {
-    twoMonitor.run();
-  } else if (S.screenCount() === 1) {
-    oneMonitor.run();
-  }
-};
+//var twoMonitor = S.op("layout", { "name" : twoMonitorLayout });
+//var oneMonitor = S.op("layout", { "name" : oneMonitorLayout });
+// var universalLayout = function() {
+//   // Should probably make sure the resolutions match but w/e
+//   if (S.screenCount() === 3) {
+//     threeMonitor.run();
+//   } else if (S.screenCount() === 2) {
+//     twoMonitor.run();
+//   } else if (S.screenCount() === 1) {
+//     oneMonitor.run();
+//   }
+// };
 
 // Batch bind everything. Less typing.
 S.bnda({
   // Layout Bindings
-  "padEnter:ctrl" : universalLayout,
-  "space:ctrl" : universalLayout,
+  //"padEnter:ctrl" : universalLayout,
+  //"space:ctrl" : universalLayout,
 
   // Basic Location Bindings
   "pad0:ctrl" : lapChat,
   "[:ctrl" : lapChat,
   "pad.:ctrl" : lapMain,
   "]:ctrl" : lapMain,
-  "pad1:ctrl" : tboltBottomLeft,
-  "pad2:ctrl" : tboltBottomMid,
-  "pad3:ctrl" : tboltBottomRight,
-  "pad4:ctrl" : tboltLeft,
-  "pad5:ctrl" : tboltFull,
-  "pad6:ctrl" : tboltRight,
-  "pad7:ctrl" : tboltTopLeft,
-  "pad8:ctrl" : tboltTop,
-  "pad9:ctrl" : tboltTopRight,
-  "pad=:ctrl" : tboltTop,
-  "pad/:ctrl" : tboltBottom,
-  "pad*:ctrl" : hpBottomLeft,
-  "pad-:ctrl" : hpTopLeft,
-  "pad+:ctrl" : hpRight,
+  "pad1:ctrl" : lgBottomLeft,
+  "pad2:ctrl" : lgBottomMid,
+  "pad3:ctrl" : lgBottomRight,
+  "pad4:ctrl" : lgLeft,
+  "pad5:ctrl" : lgFull,
+  "pad6:ctrl" : lgRight,
+  "pad7:ctrl" : lgTopLeft,
+  "pad8:ctrl" : lgTop,
+  "pad9:ctrl" : lgTopRight,
+  "pad=:ctrl" : lgTop,
+  "pad/:ctrl" : lgBottom,
+  "pad*:ctrl" : lgBottomLeft,
+  "pad-:ctrl" : lgTopLeft,
+  "pad+:ctrl" : lgRight,
 
   // Resize Bindings
   // NOTE: some of these may *not* work if you have not removed the expose/spaces/mission control bindings
